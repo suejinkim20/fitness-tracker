b837313b16d436e64fe1bd96404c864f69c6591b
Suejin Kim <kim.suejin.20@gmail.com>
2021-06-13 15:55:41 -0400

    added model/schema for workouts

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+            },
+            name: {
+                type: String,
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        }
+    ],
+    combinedWeight: {
+        type: Number,
+    },
+    totalDuration: {
+        type: Number,
+    },
+
+})
---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+//require model(s) here
+
+router.post()
+
+router.get()
+
+
+module.exports = router;
+
+
+
+
