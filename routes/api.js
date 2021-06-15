af26e648c1590fbb7c4e7e9ff9707c89d6e50094
Suejin Kim <kim.suejin.20@gmail.com>
2021-06-14 23:26:13 -0400

    mongodb connect correctly and one get route running correctly

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,46 @@
 const router = require("express").Router();
-//require model(s) here
+const Workout = require('../models/workout.js')
 
-router.post()
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
 
-router.get()
+})
+
+router.post("/api/workout/bulk", ({ body }, res) => {
+    workout.insertMany(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+  
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}, (error, data) => {
+        if (error) {
+            res.send(error)
+        } else {
+            res.json(data)
+        }
+    })
+    //   .sort({ date: -1 })
+    //   .then(dbWorkout => {
+    //     res.json(dbWorkout);
+    //   })
+    //   .catch(err => {
+    //     res.status(400).json(err);
+    //   });
+});
+  
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan")
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,13 +13,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/-", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
