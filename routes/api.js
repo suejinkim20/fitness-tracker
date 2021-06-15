de474e9693cc26befa4b1dacb98767d3554f1bc4
Suejin Kim <kim.suejin.20@gmail.com>
2021-06-15 00:41:51 -0400

    added route for stats dashboard

--- routes/api.js
+++ routes/api.js
@@ -15,16 +15,9 @@ router.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
 
+//api.js routes
 router.get("/api/workouts", (req, res) => {
-    Workout.find({}
-    //     , (error, data) => {
-    //     if (error) {
-    //         res.send(error)
-    //     } else {
-    //         res.json(data)
-    //     }
-    // }
-    )
+    Workout.find({})
       .sort({ date: -1 })
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -34,30 +27,15 @@ router.get("/api/workouts", (req, res) => {
       });
 });
 
-
-router.post("/api/workouts", ({body}, res) => {
-    Workout.create(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout)
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        })
-
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
 })
-
-{ //Do i even need this route since i ran npm run seed through seeds.js?
-// router.post("/api/workout/bulk", ({ body }, res) => {
-//     workout.insertMany(body)
-//       .then(dbWorkout => {
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-// });
-}
-
 
 
 module.exports = router;
