91391fe74e3c158b1cdeaea10ffc0115606f6a32
Suejin Kim <kim.suejin.20@gmail.com>
2021-06-14 23:55:36 -0400

    added path npm package and linked stats and exercise pages

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,39 @@
 const router = require("express").Router();
-const Workout = require('../models/workout.js')
+const Workout = require("../models/workout.js")
+const path = require("path")
+
+//HTML Routes
+router.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+router.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+
+router.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}
+    //     , (error, data) => {
+    //     if (error) {
+    //         res.send(error)
+    //     } else {
+    //         res.json(data)
+    //     }
+    // }
+    )
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
 
 router.post("/api/workouts", ({body}, res) => {
     Workout.create(body)
@@ -12,34 +46,17 @@ router.post("/api/workouts", ({body}, res) => {
 
 })
 
-router.post("/api/workout/bulk", ({ body }, res) => {
-    workout.insertMany(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-});
-  
-
-router.get("/api/workouts", (req, res) => {
-    Workout.find({}, (error, data) => {
-        if (error) {
-            res.send(error)
-        } else {
-            res.json(data)
-        }
-    })
-    //   .sort({ date: -1 })
-    //   .then(dbWorkout => {
-    //     res.json(dbWorkout);
-    //   })
-    //   .catch(err => {
-    //     res.status(400).json(err);
-    //   });
-});
-  
+{ //Do i even need this route since i ran npm run seed through seeds.js?
+// router.post("/api/workout/bulk", ({ body }, res) => {
+//     workout.insertMany(body)
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
+// });
+}
 
 
 
